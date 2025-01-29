d2827ae555569736147679ff8ae1ad720e6ed840
malu-estevam <malu.estevam@ucas-edu.net>
2025-01-29 09:52:35 -0700

    python practice

---
+++ Variables/practice.py
@@ -0,0 +1,41 @@
+# Malu Estevam, Variables Practice Python
+
+name = "My name is Malu Estevam"
+
+print(name)
+
+number = "2"
+
+print(number)
+
+number2 = "144"
+
+print(number2)
+
+breakfast = "I ate nothing this morning"
+
+print(breakfast)
+
+color = "pink and white are my favorite colors"
+
+print(color)
+
+school = "I go to UCAS"
+
+print(school)
+
+year = "It is 2025"
+
+print(year)
+
+eye_color = "my eyes are dark brown"
+
+print(eye_color)
+
+age = "I am 14 years old"
+
+print(age)
+
+favorite_subject = "I love math and biology"
+
+print(favorite_subject)
