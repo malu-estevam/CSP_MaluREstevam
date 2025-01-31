bc8a50f2b9ec96b65e9543d2a298a38ad1368e48
malu-estevam <malu.estevam@ucas-edu.net>
2025-01-31 09:24:49 -0700

    Variables Practice C

---
+++ practice.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+char name[] = "Malu";
+int num = 23;
+int num2 = 543;
+char breakfast[] = "cherries";
+char color[] = "pink";
+char school[] = "UCAS";
+char eye[] = "brown";
+int age = 14;
+char subject[] = "math";
+
+int main(void){
+    printf("%s\n", name);
+    printf("%d\n", num);
+    printf("%d\n", num2);
+    printf("%s\n", breakfast);
+    printf("%s\n", color);
+    printf("%s\n", school);
+    printf("%s\n", eye);
+    printf("%d\n", age);
+    printf("%s\n", subject);
+    return 0;
+}
