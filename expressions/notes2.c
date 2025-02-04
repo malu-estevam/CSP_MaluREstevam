e09ecb4251070d2b7ec6bcb7feddbf1837204b5a
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-04 09:48:17 -0700

    Expression practice for python

--- expressions/notes2.c
+++ expressions/notes2.c
@@ -1,18 +1,34 @@
 //Malu Estevam, Expressions Notes C
 #include <stdio.h>
+#include <math.h> //this is what lets us do exponents
 // integers have int when we set the variable and %d when we print
 // floats have float when we set and have %f when we print
 // strings (words) have char when we set the variable and %s when we print
 int mynum;
 float percent;
+int add = 4+6;
+int sub = 4-6;
+int mul = 4*6;
+float div = 6/4;
+int mod = 6%4;
+int ex = pow(5, 2);
+
 
 int main(void){
 
-    printf("type a number: \n");
-    scanf("%d", &mynum);
-    printf("You inputed %d\n", mynum);
-    printf("Give me a percent as a decimal: \n");
-    scanf("%f", &percent);
-    printf("Your percent is %f\n", percent);
+    //printf("type a number: \n");
+    //scanf("%d", &mynum);
+    //printf("You inputed %d\n", mynum);
+    //printf("Give me a percent as a decimal: \n");
+    //scanf("%f", &percent);
+    //printf("Your percent is %f\n", percent);
+    
+    printf("%d\n", add);
+    printf("%d\n", sub);
+    mul = 7*4; // reassigning a variable has to be inside main
+    printf("%d\n", mul);
+    printf("%.2f\n", div); // number after percent tells computer how many points we want afterwards
+    printf("%d\n", mod);
+    printf("%d\n", ex);
     return 0;
 }
--- expressions/notes2.py
+++ expressions/notes2.py
@@ -11,3 +11,5 @@ print(10.5//5) #whole number division
 print(10.5%5) #Modulo/Modulus/MOD Gives the remainder
 
 print((3+7)*2)
+pi = 3.141592653
+print(f"{pi:.4f}") #f strings are to round up the decimal place you want it to 
