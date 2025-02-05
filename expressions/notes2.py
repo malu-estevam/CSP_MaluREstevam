814c9b88bee2f9a089de04f1b365be1834bacd3f
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-05 09:48:12 -0700

    added how to round in py

--- expressions/notes2.py
+++ expressions/notes2.py
@@ -13,3 +13,4 @@ print(10.5%5) #Modulo/Modulus/MOD Gives the remainder
 print((3+7)*2)
 pi = 3.141592653
 print(f"{pi:.4f}") #f strings are to round up the decimal place you want it to 
+print(round(pi, 4)) #round is used to round up and you put how many variable you want
