f23306d0e6b03ff9bbe0ae17a21d3fe90cc1b824
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-07 09:55:21 -0700

    Started C financial Calculator

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -1,9 +1,13 @@
 //Malu Estevam, Financial Calculator C
 #include <stdio.h>
-
-
-
+char welcome[] = "Welcome to your very own financial calculator. Follow the instructions below";
+float income;
+float spend;
 int main(void){
-    printf("Hello World");
+    printf("%s\n", welcome);
+    printf("%s\n", "What is your monthly income?");
+    scanf("%s\n", income);
+    printf("%s\n", "How much do you spend?");
+    scanf("%s\n", spend);
     return 0;
 }
