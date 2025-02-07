e59ca0ff83c2ed07b51764fa539744fc605082e1
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-07 16:38:26 -0700

    continued financial calc in c

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -1,5 +1,6 @@
 //Malu Estevam, Financial Calculator C
 #include <stdio.h>
+#include <math.h>
 char welcome[] = "Welcome to your very own financial calculator. Follow the instructions below";
 float income;
 
@@ -33,6 +34,15 @@ int main(void){
 
     printf("%s\n", "What is the cost of your transportation?");
     scanf("%f\n", transportation);
+
+    float savings = income/10;
+    float spend_percent = income - (rent+utilities+groceries+transportation+savings)*100;
+    float rent_percent = (rent/income)*100;
+    float utilities_percent = (utilities/income)*100;
+    float groceries_percent = (groceries/income)*100;
+    float transportation_percent = (transportation/income)*100;
+
+    printf("You spend $%f on rent and that is %f percent your income", rent, rent_percent);
 
 
 
