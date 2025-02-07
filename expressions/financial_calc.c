f1458e1d97f21f94a6aa08aa173b41ff6a1cc22e
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-07 22:35:40 +0000

    fixed my financial calc in c

--- expressions/financial_calc.c
+++ expressions/financial_calc.c
@@ -17,22 +17,24 @@ int main(void){
     printf("%s\n", welcome);
 
     printf("%s\n", "What is your monthly income?"); 
-    scanf("%s\n", income);
+    scanf("%f\n", income);
     
     printf("%s\n", "How much do you spend?");
-    scanf("%s\n", spend);
+    scanf("%f\n", spend);
 
     printf("%s\n", "What is your rent amount?");
-    scanf("%s\n", rent);
+    scanf("%f\n", rent);
 
     printf("%s\n", "What is the cost of your utilities?");
-    scanf("%s\n", utilities);
+    scanf("%f\n", utilities);
 
     printf("%s\n", "What is the cost of your groceries?");
-    scanf("%s\n", groceries);
+    scanf("%f\n", groceries);
 
     printf("%s\n", "What is the cost of your transportation?");
-    scanf("%s\n", transportation);
+    scanf("%f\n", transportation);
+
+
 
     return 0;
 }
