a0d184cfcab50bf3ccc136f3654f787c293a721f
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-10 09:53:21 -0700

    started string notes

---
+++ strings/stringnotes.py
@@ -0,0 +1,15 @@
+# Malu Estevam, Strings Notes Python
+
+# A string is words in programming (data type) "" ''
+
+# print("This is Malu's code")
+
+# name = input("What is your name?\n").strip().lower()
+
+# print(f"Hello {name} welcome to my program.")
+
+# name = input("Please give me a number\n").strip().capitalize()
+
+sentence = "The quick brown fox jumps over the lazy dog"
+
+print(sentence.find("fox"))
