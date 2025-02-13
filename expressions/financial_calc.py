7a0fc684787b78121cbcbbe6dcc75503e5f87ac9
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-13 09:45:03 -0700

    Financial Calc C

--- expressions/financial_calc.py
+++ expressions/financial_calc.py
@@ -4,8 +4,6 @@
 print("Welcome to your very own financial calculator. Follow the instructions below")
 # Ask for monthly income (user input) 
 income = float(input("What is your monthly income\n"))
-# Ask for spending amount
-spend = float(input("How much do you spend\n"))
 # Ask for rent amount (user input) 
 rent = float(input("What is your rent amount\n"))
 # Ask for utilities amount (user input) 
@@ -17,7 +15,7 @@ transportation = float(input("What is the cost of your transportation\n"))
 # Calculate savings as 10% of income (variable)
 savings = float(income/10)
 # Calculate spending money income - (rent+utilities+groceries+transportation+savings)*100 (variable)
-spend_percent = (income - (rent+utilities+groceries+transportation+savings))*100
+spend = (income - (rent+utilities+groceries+transportation+savings))
 # Calculate percent of rent (rent/income)*100 (variable)
 rent_percent = (rent/income)*100
 # Calculate percent of utilities (utilities/income)*100 (variable)
@@ -37,6 +35,6 @@ print(f"You spend ${groceries:.2f} on groceries and that is {groceries_percent:.
 # Tell user category spending amount and percent for transportation ("You spend $XX.XX on transportation and that is XX% of your income")
 print(f"You spend ${transportation:.2f} on transportation and that is {transportation_percent:.2f}% of your income")
 # Tell user category spending amount and percent for spending ("You spend $XX.XX on spending and that is XX% of your income")
-print(f"You spend ${spend:.2f} on spending and that is {spend_percent:.2f}% of your income")
+print(f"You have ${spend:.2f} left for spending and that is {spend_percent:.2f}% of your income")
 # Tell user category spending amount and percent for savings ("You spend $XX.XX on savings and that is 10% of your income")
 print(f"You have ${savings:.2f} left for savings and that is 10% of your income")
