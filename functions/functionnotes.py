1e0938446cf83423cb2ee775bd61eaf32a20717b
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-19 09:43:14 -0700

    Updated Hello World Python

--- functions/functionnotes.py
+++ functions/functionnotes.py
@@ -2,23 +2,23 @@
 
 # A function is an action stored in a keyword
 
-# number = int(input("Can I get a number?:\n")) # How to make parameter while also keeping user input
-# def add(numOne, numTwo): 
-    # print(numOne+numTwo)
+number = int(input("Can I get a number?:\n")) # How to make parameter while also keeping user input
+def add(numOne, numTwo): 
+    print(numOne+numTwo)
 
-# add(int(input("tell me a number\n")), number)
+add(int(input("tell me a number\n")), number)
 
-# def add():
-    # numOne = int(input("Give me a number:\n"))
-    # numTwo = int(input("Give me a number:\n"))
-    # print(numOne+numTwo)
+def add():
+    numOne = int(input("Give me a number:\n"))
+    numTwo = int(input("Give me a number:\n"))
+    print(numOne+numTwo)
 
-# add()
+add()
 
-# def add(numOne, numTwo): # Parameters go in the parentheses separated by commas
-    # print(numOne+numTwo)
+def add(numOne, numTwo): # Parameters go in the parentheses separated by commas
+    print(numOne+numTwo)
 
-# add(12, 20) # Arguments are given when the function is called AND must match the number of parameters
+add(12, 20) # Arguments are given when the function is called AND must match the number of parameters
 
 def user(word): 
     return input(f"Tell me a {word}:\n")
---
+++ functions/updated_hello_world.py
@@ -0,0 +1,11 @@
+# Malu Estevam, Updated Hello World Python
+
+name = input("Please tell me a name:\n")
+def user(name):
+    print(f"Hello {name}")
+
+user(name)
+user("Maehva")
+user("Mili")
+user("James")
+user("Jackie")
