2e150896d866213f2bb8044bcd2096c74a1cc5ed
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-19 09:54:51 -0700

    Updated Hello World C

---
+++ functions/updated_hello_world.c
@@ -0,0 +1,10 @@
+//Malu Estevam, Updated Hello World
+#include <stdio.h>
+
+
+int main(void){
+    char name[50];
+    printf("Please tell me your name:\n");
+    scanf("%s", name);
+    return 0;
+}
