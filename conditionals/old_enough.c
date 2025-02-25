6553eaf2a61bf3d7e08801d2cde9d2c56a5c1bf3
malu-estevam <malu.estevam@ucas-edu.net>
2025-02-25 15:51:23 -0700

    finished old enough for C

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -1,20 +1,20 @@
 // Malu Estevam, old enough C
 #include <stdio.h>
 #include <string.h>
-int age = 18;
+int age = 17;
 
 
 int main(void){
-    if(age == 18){
+    if(age >= 18){
         printf("You are old enough to vote");
     }
-    else if(age == 16){
+    else if(age >= 16){
         printf("You are old enough to drive");
     }
-    else if(age == 15){
+    else if(age >= 15){
         printf("You are old enough to get a learners permit");
     }
-    else if(age == 5){
+    else if(age >= 5){
         printf("You are old enough to go to school");
     }else{
         printf("You are too young to do anything");
