997280e9bbda91fa7594956b2f48c4826f92bdec
malu-estevam <malu.estevam@ucas-edu.net>
2025-03-04 00:49:53 +0000

    fixed fizzbuzz assignment py

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,9 +1,11 @@
 # Malu Estevam, fizzbuzz Python
 for x in range(1,51):
-    if i % 3:
-    print("fizz")
-    if i % 5:
-    print("buzz")
-    if i % 3 and i % 5:
-    print("fizzbuzz")
+    if x % 3:
+        print("fizz")
+    if x % 5:
+        print("buzz")
+    if x % 3 and x % 5:
+        print("fizzbuzz")
+    else:
+        print(x)
     
