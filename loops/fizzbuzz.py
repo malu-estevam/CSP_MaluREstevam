5d4dc50f331b076c5b5d4e03dfe804f98a877fb1
malu-estevam <malu.estevam@ucas-edu.net>
2025-03-05 20:29:38 +0000

    fixed code for fizzbuzz

--- loops/fizzbuzz.py
+++ loops/fizzbuzz.py
@@ -1,11 +1,11 @@
 # Malu Estevam, fizzbuzz Python
-for x in range(1,51):
-    if x % 3 == 0:
+for x in range(0,51):
+    if x % 3 == 0 and x % 5 == 0:
+        print("fizzbuzz")
+    elif x % 3 == 0:
         print("fizz")
     elif x % 5 == 0:
         print("buzz")
-    elif x % 3 == 0 and x % 5 == 0:
-        print("fizzbuzz")
     else:
         print(x)
     
