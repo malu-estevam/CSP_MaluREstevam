24619ca90d1c84b9fdbb10f8c13b472343d17b6f
malu-estevam <malu.estevam@ucas-edu.net>
2025-03-05 20:39:44 +0000

    finished fizzbuzz in c

--- loops/fizzbuzz.c
+++ loops/fizzbuzz.c
@@ -5,7 +5,20 @@ int x;
 
 int main(void){
     for(x=0;x<51;x++){
-        if 
+        if (x % 3 == 0 && x % 5 == 0){
+            printf("fizzbuzz\n");
+
+        }
+        else if (x % 3 == 0){
+            printf("fizz\n");
+
+        }
+        else if (x % 5 ==0){
+            printf("buzz\n");
+
+        }else{
+            printf("%d\n", x);
+        }
     }
     return 0;
 }
