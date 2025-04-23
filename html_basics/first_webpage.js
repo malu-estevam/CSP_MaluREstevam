c8c6e4ffa7289c9f3cde179c9f10c6860461dd0f
malu-estevam <malu.estevam@ucas-edu.net>
2025-04-23 16:34:41 -0600

    finished updating my first website

---
+++ html_basics/first_webpage.js
@@ -0,0 +1,14 @@
+let images = ["https://abbasite.com/uploads/2018/04/story-image-abba-1024x662.jpg"]
+let count = 0
+function show(){
+    document.getElementById("click").style.display = "block"
+}
+
+function change(){
+    document.getElementById("abba").src = images[count]
+    if(count === 2){
+        count = 0
+    }else{
+        count += 1
+    }
+}
