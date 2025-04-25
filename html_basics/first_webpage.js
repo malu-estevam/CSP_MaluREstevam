38e6ae0df18707f645640441d284bb16aeda2a41
malu-estevam <malu.estevam@ucas-edu.net>
2025-04-25 09:15:09 -0600

    fixed my first webpage

--- html_basics/first_webpage.js
+++ html_basics/first_webpage.js
@@ -1,4 +1,4 @@
-let images = ["https://abbasite.com/uploads/2018/04/story-image-abba-1024x662.jpg", "https://youth.europa.eu/sites/default/files/nnfe-people/8.jpg" ]
+let images = ["https://abbasite.com/uploads/2018/04/story-image-abba-1024x662.jpg", "https://youth.europa.eu/sites/default/files/nnfe-people/8.jpg", "https://people.com/thmb/47fLRUa6Xnp5VOB2UDc14xqG5gc=/4000x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(1014x389:1016x391):format(webp)/Adele-Pride-060224-03-8617463cbd8042209885c4de8dab4996.jpg" ]
 let count = 0
 function show(){
     document.getElementById("click").style.display = "block"
@@ -11,4 +11,14 @@ function change(){
     }else{
         count += 1
     }
+}
+function show(){
+    if(document.getElementById("click").style.display != "flex"){
+        document.getElementById("click").style.display = "flex"
+    document.getElementById("btn").innerHTML = "Show less"
+    }else{
+        document.getElementById("click").style.display = "none"
+    document.getElementById("btn").innerHTML = "Show more"
+    }
+    
 }
