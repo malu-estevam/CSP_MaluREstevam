252d76b2e4d057f070eabcee49781e9a88b33f85
malu-estevam <malu.estevam@ucas-edu.net>
2025-04-28 09:52:44 -0600

    worked on final

--- html_basics/final_project.js
+++ html_basics/final_project.js
@@ -1,10 +1,19 @@
 function show(){
-    if(document.getElementById("click").style.display != "flex"){
-        document.getElementById("click").style.display = "flex"
-    document.getElementById("btn").innerHTML = "Show less"
+    if(document.getElementById("click").style.display != "block"){
+        document.getElementById("click").style.display = "block"
+        document.getElementById("btn").innerHTML = "Show less"
+        
     }else{
         document.getElementById("click").style.display = "none"
-    document.getElementById("btn").innerHTML = "Show more"
+        document.getElementById("btn").innerHTML = "Show more"
     }
     
+}
+function highlight(){
+    document.getElementById("btn").style.backgroundColor = "rgb(201, 95, 83)"
+    document.getElementById("btn").style.color = "white"
+}
+function normal(){
+    document.getElementById("btn").style.backgroundColor = "gray"
+    document.getElementById("btn").style.color = "black"
 }
