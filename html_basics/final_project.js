412ae451c508ed24a1ab89ba356fe510c914beac
malu-estevam <malu.estevam@ucas-edu.net>
2025-04-29 09:37:50 -0600

    worked on final

--- html_basics/final_project.js
+++ html_basics/final_project.js
@@ -1,3 +1,13 @@
+let images = ["https://www.nps.gov/npgallery/GetAsset/3985E32C-1DD8-B71B-0BC98AEE806D3218/proxy/hires", "https://www.nps.gov/arch/planyourvisit/images/14001330199_c3c118e4c7_k.jpg" ]
+let count = 0
+function change(){
+    document.getElementById("img2").src = images[count]
+    if(count === 2){
+        count = 0
+    }else{
+        count += 1
+    }
+}
 function show(){
     if(document.getElementById("click").style.display != "block"){
         document.getElementById("click").style.display = "block"
@@ -17,3 +27,4 @@ function normal(){
     document.getElementById("btn").style.backgroundColor = "gray"
     document.getElementById("btn").style.color = "black"
 }
+
